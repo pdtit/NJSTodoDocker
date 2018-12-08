eba2ed0b89af962c48fda14d1623ec17763c6d7a
pdetender <peter@pdtit.be>
2018-12-08 10:23:42 -0600

    v1.0
    sample NodeJS Docker Container

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+    remoteUrl : 'mongodb://cosmosdbshell:KDhzf1Q9uNdctNxiPlQmOws9r5BiH03y9ZMmuwRqq1ufMacJEDu4dMaaTdNzZA2BHtdMMkFuBMNFkcInf88KVw==@cosmosdbshell.documents.azure.com:10255/?ssl=true&replicaSet=globaldb',
+    
+    localUrl: 'mongodb://localhost/meanstacktutorials'
+};
